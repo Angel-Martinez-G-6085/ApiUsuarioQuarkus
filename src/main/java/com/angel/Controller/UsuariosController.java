726362af4de7aa4830c4cde2c27b41dbf529c800
Angel Ivan Martinez <ben6085@gmail.com>
2024-12-20 20:25:14 -0600

    Se agrego un archivo de logs junto con las configuraciones para crearlos

--- src/main/java/com/angel/Controller/UsuariosController.java
+++ src/main/java/com/angel/Controller/UsuariosController.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
 import com.angel.Entity.Usuario;
 import com.angel.Responses.CreateUserResponse;
@@ -23,18 +24,23 @@ import com.angel.service.usuarioService;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UsuariosController {
+
+    private Logger LOGGER = Logger.getLogger(UsuariosController.class);
+
     @Inject
     usuarioService usuariServicio;
 
     @GET
     @Operation(summary = "Obtener todos los usuarios", description = "Retorna una lista con todos los usuarios registrados")
     public List<Usuario>obtenerUsuarios(){
+        LOGGER.info("Se listaron todos los usuarios en la base de datos");
         return usuariServicio.getAllUsuarios();
     }
 
     @POST
     @Operation(summary = "Crear un nuevo usuario", description = "Crea un nuevo usuario y lo guarda en la base de datos")
     public CreateUserResponse createUsuario(Usuario user){
+        LOGGER.info("Se creo un nuevo usuario en la base de datos");
         return usuariServicio.createUser(user);
     }
 }
